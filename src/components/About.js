1009d3263669f6db1600a2848c5224c526c8017c
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-24 08:14:37 -0400

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div>
+    <h2>About Me</h2>
+    <p>I'm an engineer.</p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
