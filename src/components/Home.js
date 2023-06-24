172734458fc258fa0f1746ca482ea670a2ff9bce
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-24 09:09:22 -0400

    fixed typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 
-
 function Home() {
   // update the JSX being returned!
   return (
   <div id="home">
-    <h1 style={{color: "firebrick"}}>{name} is a Web Developer from Your {city}</h1>
+    <h1 style={{ color:'firebrick' }}>{ name } is a Web Developer from { city }</h1>
   </div>
   );
 }
